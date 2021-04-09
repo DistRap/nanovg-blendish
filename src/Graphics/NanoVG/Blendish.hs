84235470168560a10c802cf7759ce9c7ada05990
Richard Marko <srk@48.io>
2021-04-09 13:30:53 +0200

    better renderUI demo

--- src/Graphics/NanoVG/Blendish.hs
+++ src/Graphics/NanoVG/Blendish.hs
@@ -39,7 +39,7 @@ import Graphics.NanoVG.Blendish.Icon
 import Graphics.NanoVG.Blendish.Context
 import Graphics.NanoVG.Blendish.Monad
 import Graphics.NanoVG.Blendish.Monad.Primitives
---import Graphics.NanoVG.Blendish.Shorthand
+import Graphics.NanoVG.Blendish.Shorthand
 import Graphics.NanoVG.Blendish.Types
 import Graphics.NanoVG.Blendish.Theme
 import Graphics.NanoVG.Blendish.Utils
@@ -115,13 +115,14 @@ renderUI ctx (UIData _ icons) x y mb = do
   NanoVG.fontSize ctx 18
   NanoVG.fontFace ctx "sans"
 
-  NanoVG.globalAlpha ctx 0.9
+  NanoVG.globalAlpha ctx 1
 
   _w <- flip runReaderT (UIContext ctx (defTheme icons) (x, y)) $ do
     let sp = bndWidgetHeight + 5
-
     Theme{..} <- theme
-    background (V2 0 0) (V2 1024 (100 * sp)) tBg -- (rgbaf 0.9 0.9 0.9 0.9) -- tBg
+    background (V2 0 0) (V2 600 600) tBg
+    splitter (V2 0 0) (V2 600 600)
+
     label (V2 0 bndWidgetHeight) (V2 100 10) Nothing "Demo Label"
 
     toolButton (V2 100 10) (V2 200 21)
@@ -157,13 +158,12 @@ renderUI ctx (UIData _ icons) x y mb = do
       HasFocus "Focused Option"
     optionButton (V2 100 (10 + (10*sp))) (V2 200 bndWidgetHeight)
       ActiveFocus "Active option"
-
-    let row2 = 330
-    slider (V2 row2 (10 + (0*sp))) (V2 200 bndWidgetHeight)
+    let row2 = 100
+    slider (V2 row2 (10 + (11*sp))) (V2 200 bndWidgetHeight)
       (pure True) NoFocus 0.5 "Default slider" "0.5"
-    slider (V2 row2 (10 + (1*sp))) (V2 200 bndWidgetHeight)
+    slider (V2 row2 (10 + (12*sp))) (V2 200 bndWidgetHeight)
       (pure True) HasFocus 0.7 "Focused slider" "0.7"
-    slider (V2 row2 (10 + (2*sp))) (V2 200 bndWidgetHeight)
+    slider (V2 row2 (10 + (13*sp))) (V2 200 bndWidgetHeight)
       (pure True) ActiveFocus 0.9 "Active slider" "0.9"
 
     scrollbar (V2 20 30) (V2 10 100)
@@ -173,30 +173,77 @@ renderUI ctx (UIData _ icons) x y mb = do
     scrollbar (V2 60 30) (V2 10 100)
       ActiveFocus 0.5 0.4
 
-    tooltipBackground (V2 10 150) (V2 60 60)
+    --tooltipBackground (V2 10 150) (V2 60 60)
+    tooltip (V2 10 150) (V2 120 20) "This is a tooltip"
 
-    menuBackground (V2 10 230) (V2 60 60) (pure True)
+    menuBackground (V2 260 230) (V2 120 100) (pure True)
+    menuLabel (V2 260 230) (V2 150 bndWidgetHeight) (Just Icon'MenuPanel) "Menu label"
+    menuItem (V2 260 (bndWidgetHeight - 2 + 230)) (V2 120 bndWidgetHeight) (Just Icon'Blender) NoFocus "Menu item"
+    menuItem (V2 260 (2*(bndWidgetHeight - 2) + 230)) (V2 120 bndWidgetHeight) (Just Icon'Trash) HasFocus "Focused"
+    menuItem (V2 260 (3*(bndWidgetHeight - 2) + 230)) (V2 120 bndWidgetHeight) (Just Icon'Options) ActiveFocus "Active"
 
-  let b = Button (ico Icon'Speaker $ label' "Up" $ defA ()) -- (Just Icon'Speaker) "Count up"
-      c = Button (defA () & ico Icon'Dot & label' "Down") -- (Just Icon'Dot) "Count down"
+    --coloredNodeWire (V2 160 460) (V2 200 440) red green
+    nodeWire (V2 160 460) (V2 200 440) NoFocus ActiveFocus
 
-  _zz <- newTVarIO $ HBox [
-           Rekt 30 100 100 21 b
-         , Rekt 127 100 100 21 c
-         ]
+    nodeBackground (V2 200 400) (V2 150 180)
+      NoFocus (Just Icon'ArrowLeftright) "Translate" (rgbf1 0.392) True
 
-  -- bricklike
-  -- State -> Event -> State
-  -- State -> UI
+    label (V2 200 440) (V2 30 10) Nothing "In"
+    label (V2 280 460) (V2 100 10) Nothing "Out"
+    nodePort (V2 200 440) NoFocus (rgbf1 0.5)
+    nodePort (V2 350 460) NoFocus (rgbf1 0.5)
 
-  _w <- flip runReaderT (UIContext ctx (defTheme icons) (x, y)) 
-    $ flip runStateT (defRS x y mb) $ do
-       drawElem $
-         HBox [
-           Rekt 30 300 100 21 b
-         , Rekt 127 300 100 21 c
-         ]
-  --print (show $ map fst $ hs b, show $ events $ snd w)
+    numberField (V2 220 (480 + (0*sp))) (V2 100 bndWidgetHeight)
+      (pure True) HasFocus "X" "0"
+
+    numberField (V2 220 (480 + (1*sp))) (V2 100 bndWidgetHeight)
+      (pure True) HasFocus "Y" "10"
+
+    numberField (V2 220 (480 + (2*sp))) (V2 100 bndWidgetHeight)
+      (pure True) HasFocus "Z" "10"
+
+    nodeBackground (V2 10 400) (V2 150 180)
+      HasFocus (Just Icon'Matcube) "Cube" (rgbf1 0.392) True
+
+    numberField (V2 20 (480 + (0*sp))) (V2 120 bndWidgetHeight)
+      (pure True) HasFocus "Round" "10"
+
+    label (V2 90 460) (V2 100 10) Nothing "Out"
+    nodePort (V2 160 460) HasFocus (rgbf1 0.5)
+
+    let t = "Test text bla lorem ipsum jumps over lazy cat"
+    textField (V2 310 010) (V2 100 100) (pure True) NoFocus Nothing
+      t
+      0 42
+    caretP (V2 313 010) (V2 100 100) t
+
+    textField (V2 310 110) (V2 100 100) (pure True) NoFocus Nothing
+      t
+      1 41
+
+    textField (V2 420 010) (V2 100 bndWidgetHeight) (pure True) NoFocus Nothing
+      "Crazy cat"
+      0 0
+
+    textField (V2 420 (010 + sp)) (V2 100 bndWidgetHeight) (pure True) NoFocus Nothing
+      "Crazy fox"
+      6 6
+
+    textField (V2 420 (010 + (2 * sp))) (V2 100 bndWidgetHeight) (pure True) NoFocus Nothing
+      "Lazy cat"
+      5 5
+
+    textField (V2 420 (010 + (3 *sp))) (V2 100 (bndWidgetHeight)) (pure True) NoFocus Nothing
+      "Lazy dog"
+      8 8
+
+    textField (V2 420 (010 + (4 *sp))) (V2 100 (bndWidgetHeight)) (pure True) NoFocus Nothing
+      "Foxy cat"
+      1 7
+
+    textField (V2 420 (010 + (5 *sp))) (V2 100 (bndWidgetHeight)) (pure True) NoFocus (Just Icon'GhostEnabled)
+      "Foxy owl"
+      0 8
 
   return ()
 
